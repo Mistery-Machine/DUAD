9fa1cf7732bf9cd9af442a5a1e0df166bcfe959d
Mistery-Machine <josedaniel010146@gmail.com>
2024-10-15 22:29:24 -0600

    Changes performed

--- semana10.1/main.py
+++ semana10.1/main.py
@@ -1,33 +1,38 @@
 from menu import result
-from logic import registering_students, average_notes, calculate_top_three, display_students
+from logic import registering_students, average_notes, calculate_top_three, overall_average, display_students
 from data import export_data, import_data
 
 def main():
     print("---- Welcome to the Student Management System! ---- ")
 
+    registered_students = []
+    averages = {}
+    overall_avg = 0
+
     while True:
         try:
             student_amount = int(input("Enter the number of students you want to register: "))
-            if student_amount <= 0:
+            if student_amount < 0:
                 print("El número de estudiantes no puede ser negativo.")
             else:
                 break
         except ValueError:
             print("Por favor, ingrese un número válido.")
 
-    # Si se ingresan estudiantes válidos
+    
     if student_amount > 0:
         registered_students = registering_students(student_amount)
         averages = average_notes(registered_students)
+        overall_avg = overall_average(averages)
        
     else:
-        # Si no se registran estudiantes
+        
         registered_students = []
         averages = {}
         overall_avg = 0
         print("No se registraron estudiantes.")
 
-    # Ejecutar las demás opciones del programa
+    
     result(registered_students, averages, overall_avg)
 
 if __name__ == "__main__":
--- semana10.1/menu.py
+++ semana10.1/menu.py
@@ -1,49 +1,69 @@
-from logic import display_students, top_three, print_overall_average, calculate_top_three, average_notes,overall_average
+from logic import display_students, top_three, print_overall_average, calculate_top_three, average_notes, overall_average
 from data import import_data, export_data
-import data
 
 def menu():
     print("*** Menu Principal ***")
-    print("1.View all students ")
-    print("2.View Top 3 best students ")
-    print("3.View overall average notes ")
-    print("4.Export data to a CSV file  ")
-    print("5.Import data from the CSV file ")
-    print("6.Log out ")
+    print("1. View all students ")
+    print("2. View Top 3 best students ")
+    print("3. View overall average notes ")
+    print("4. Export data to a CSV file  ")
+    print("5. Import data from the CSV file ")
+    print("6. Log out ")
 
-def result (students, averages): 
+def result(students, averages, overall_avg): 
     while True:
         menu()
-        while True:
-            try:
-                option = int(input("Enter the number of the action you want to perform: "))
-                if option not in range(1, 7):
-                    print("Enter a correct number between 1 y 6")
-                else:
-                    break
-            except ValueError:
+        try:
+            option = int(input("Enter the number of the action you want to perform: "))
+            if option not in range(1, 7):
                 print("Enter a correct number between 1 y 6")
+                continue  
+        except ValueError:
+            print("Enter a correct number between 1 y 6")
+            continue
 
+     
         if option == 6:
-            print("Logged out succesfully.")
+            print("Logged out successfully.")
             break
-
+        
         if option == 1:
-            display_students(students)
+            if students:
+                display_students(students)
+            else:
+                print("There's no students registered.")
                 
         elif option == 2:
-            top_3_students = calculate_top_three(averages)
-            top_three(top_3_students)
-                
+            if averages:
+                top_3_students = calculate_top_three(averages)
+                top_three(top_3_students)
+            else:
+                print("There's no averages to calculate.")
+        
+
         elif option == 3:
-            print_overall_average(averages)
+            if averages:
+                print_overall_average(averages)
+            else:
+                print("There's no averages available.")
                 
+
         elif option == 4:
-            total_avg = overall_average(averages)
-            export_data(students, averages, total_avg)
+            if students and averages:
+                total_avg = overall_average(averages)
+                export_data(students, averages, total_avg)
+                print("Data exported succesfully.")
+            else:
+                print("There's no data to export.")
                   
+
         elif option == 5:
             imported_students = import_data("csv_file.csv")
             if imported_students:
-                students.extend(imported_students)
+                
+                for student in imported_students:
+                    if student not in students:
+                        students.append(student)
                 print("Students imported successfully.")
+            else:
+                print("No students were imported.")
