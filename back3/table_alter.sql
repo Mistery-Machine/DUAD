5ddab2e23b59be4b3664289a3a714a0f9343ec77
Mistery-Machine <josedaniel010146@gmail.com>
2025-11-06 11:09:38 -0600

    Delete back3 directory

--- back3/table_alter.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE invoices
-ADD COLUMN phone_number VARCHAR(20);
-
-ALTER TABLE invoices
-ADD COLUMN cashier_code VARCHAR(20);
--- back3/table_select.sql
+++
@@ -1,5 +0,0 @@
-SELECT * from invoices WHERE buyer_mail = 'juan@example.com';
-
-SELECT * from invoices ORDER BY total_amount DESC;
-
-SELECT * from invoices WHERE invoice_number =1
